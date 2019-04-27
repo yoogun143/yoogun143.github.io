c73285daaf5c785a53807af7c3378c871a8282fe
yoogun143 <thanhhl95@gmail.com>
2019-04-27 15:00:56 -0400

    form

---
+++ contact-form-handler.php
@@ -0,0 +1,23 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $email_from = 'thanhhl95@gmail.com';
+
+  $email_subject = 'New Form submission';
+
+  $email_body = "User Name: $name.\n".
+                  "User Email: $visitor_email.\n".
+                    "User Message: $message.\n";
+
+  $to = 'thanhhl95@gmail.com';
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-to: $visitor_email \r\n";
+
+  mail($to, $email_subject, $email_body, $headers);
+
+  header('Location: index.html');
+ ?>
